c5518495f90d29ad6c0fa3dbbcf7bfeb72bddbbb
Youssef Wagih <youssefwagih212@gmail.com>
2024-04-30 22:51:26 +0300

    Players can kill each other

--- Laser/laser.gd
+++ Laser/laser.gd
@@ -16,8 +16,8 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 
 
 func _on_body_entered(body):
-	if (body.is_in_group('player')):
-		print("collided with player")
+	if body is player1:
+		body.die()
 
 
 func _on_area_entered(area):
